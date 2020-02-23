c0b60419ee7b64c81418330c33208655ebff9f02
Afront <3943720+Afront@users.noreply.github.com>
2020-02-23 23:41:23 +0900

    Init project

---
+++ spec/ll_calculator_spec.cr
@@ -0,0 +1,9 @@
+require "./spec_helper"
+
+describe LlCalculator do
+  # TODO: Write tests
+
+  it "works" do
+    false.should eq(true)
+  end
+end
---
+++ src/ll_calculator.cr
@@ -0,0 +1,4 @@
+# TODO: Write documentation for `LLCalculator`
+module LLCalculator
+  VERSION = "0.1.0"
+end
